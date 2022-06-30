a06d9b7056c819f2fcb84f4493b920a821d0ffc7
Stefan Bereghici <sbereghici@gmail.com>
2022-06-30 16:06:00 +0300

    #5 - Implement Basic JWT Authorization (small changes)

--- src/main/java/com/example/springsecurityjwt/jwt/JwtConfiguration.java
+++ src/main/java/com/example/springsecurityjwt/jwt/JwtConfiguration.java
@@ -3,11 +3,11 @@ package com.example.springsecurityjwt.jwt;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
 
+@Configuration
 @ConfigurationProperties(prefix = "application.jwt")
-@Component
 @Getter
 @Setter
 public class JwtConfiguration {
@@ -20,14 +20,5 @@ public class JwtConfiguration {
 
     public String getAuthorizationHeader() {
         return HttpHeaders.AUTHORIZATION;
-    }
-
-    @Override
-    public String toString() {
-        return "JwtConfiguration{" +
-                "secretKey='" + secretKey + '\'' +
-                ", tokenPrefix='" + tokenPrefix + '\'' +
-                ", tokenExpirationAfterDays=" + tokenExpirationAfterDays +
-                '}';
     }
 }
